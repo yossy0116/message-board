96abd20c579b5b0bf8edcd0a83c8b9416258993c
yossy0116 <finance520116@gmail.com>
2016-08-07 12:24:07 +0000

    teishutu2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,7 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
 
   # 年齢は必須入力かつ0以上90以下
-  validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0 } , presence: true
+  validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than: 100 } , presence: true
 
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
